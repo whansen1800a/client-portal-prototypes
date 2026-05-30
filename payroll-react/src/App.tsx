da6601ef92434ee4a7312d3ae38c7337f2c75870
swade <swadehansen@gmail.com>
2026-05-30 09:42:23 -0600

    feat: add theme, main entry, and router

---
+++ payroll-react/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import PayrollShell from './components/shell/PayrollShell';
+import CompanyDetailsPage from './pages/CompanyDetailsPage';
+import ContractorsPage from './pages/ContractorsPage';
+import RunPayrollPage from './pages/RunPayrollPage';
+
+export default function App() {
+  return (
+    <Router>
+      <PayrollShell>
+        <Switch>
+          <Route path="/company" component={CompanyDetailsPage} />
+          <Route path="/contractors" component={ContractorsPage} />
+          <Route path="/run-payroll" component={RunPayrollPage} />
+          <Redirect from="/" to="/company" />
+        </Switch>
+      </PayrollShell>
+    </Router>
+  );
+}
