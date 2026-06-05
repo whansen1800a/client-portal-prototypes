be9f98af400b9c4c67d0de932f6509f67880f25d
swade <swadehansen@gmail.com>
2026-06-05 12:09:18 -0600

    debug: force dev React for full error messages

--- ap-ar-react/vite.config.ts
+++ ap-ar-react/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/client-portal-prototypes/ap-ar-react-build/',
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('development'),
+  },
   build: {
     outDir: '../ap-ar-react-build',
     emptyOutDir: true,
